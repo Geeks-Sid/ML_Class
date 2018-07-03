4c2ce072c0d0d23030dc934f713799186d2ef343
Siddhesh Thakur <sid.cre8er@gmail.com>
2018-07-03 17:06:30 +0530

    Update knn.py

--- Prac_1_KNN/knn.py
+++ Prac_1_KNN/knn.py
@@ -1,13 +1,20 @@
-from keras.datasets import mnist
+fil_loc = r'C:\Users\oslab\Downloads\mnist.pkl.gz'
+import pickle
+#train_set, valid_set, test_set = pickle.load(fil_loc)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-import numpy as np
-
-x_train = np.resize(x_train, (60000,784))
-x_test = np.resize(x_test, (10000, 784))
-
+import sys
+import gzip
+with gzip.open(fil_loc, 'rb') as f:
+    if sys.version_info.major > 2:
+        train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+    else:
+        train_set, valid_set, test_set = pickle.load(f)
 from sklearn.neighbors import KNeighborsClassifier
+
+x_train = train_set[0]
+y_train = train_set[1]
+x_test = test_set[0]
+y_test = test_set[1]
 
 clf = KNeighborsClassifier(n_neighbors = 3)
 clf.fit(x_train, y_train)
