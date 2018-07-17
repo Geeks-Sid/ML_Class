63d52b0294e17cbbb4c0c722d6befc7a0fb307ff
Siddhesh Thakur <sid.cre8er@gmail.com>
2018-07-17 18:16:39 +0530

    Add files via upload

---
+++ DTC/Decision_Tree.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 16:23:33 2018
+
+@author: cnlab
+"""
+
--- DTC/dtc_wosk.py
+++ DTC/dtc_wosk.py
@@ -1,19 +1,52 @@
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+column = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'status']
+df = pd.read_csv('/home/cnlab-14/Documents/DTC/car.data', header = None)
+df.columns = column
+import math
 
-def entropy(attribute_data):
-    _, val_freqs = np.unique(attribute_data, return_counts=True)
-    val_probs = val_freqs / len(attribute_data)
-    return -val_probs.dot(np.log(val_probs))
+labels = df['status']
+data = df.iloc[:, :-1]
 
-def get_count_dict(data):
-    data_values, data_freqs = np.unique(data, return_counts=True)
-    return dict(zip(data_values, data_freqs))
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.3, random_state=42)
 
-def info_gain(attribute_data, labels):
+count_unacc = df[df['status'] == 'unacc'].count()
+count_acc = df[df['status'] == 'acc'].count()
+count_good = df[df['status'] == 'good'].count()
+count_vgood = df[df['status'] == 'vgood'].count()
 
-    attr_val_counts = get_count_dict(attribute_data)
-    total_count = len(labels)
-    EA = 0.0
-    for attr_val, attr_val_count in attr_val_counts.items():
-        EA += attr_val_count * entropy(labels[attribute_data == attr_val])
-    return entropy(labels) - EA / total_count
+counts = {'unacc':count_unacc[0], 'acc':count_acc[0], 'good':count_good[0],
+          'vgood':count_vgood[0]}
+def entropy():
+    return
+
+def get_col_entropy():
+    return
+
+def get_mul_log(n, k):
+    return -(n/k)*(math.log2(n/k))
+
+def tot_entropy(counts_dict):
+    x = 0
+    tot_count = sum(counts_dict.values())
+    for key in counts_dict:
+        x += get_mul_log(counts_dict[key], tot_count)
+    return x
+
+tot_entropy_val = tot_entropy(counts)
+
+m = {}
+for c in column:
+    for key in counts:
+        x = df[df[c] == key].count()
+        m[key+'_'+c] = x[0]
+        print(c)
+def gain(some_list):
+    yy
+    
+
+#
+#df[(df['persons'] == '2') & (df['status'] == 'unacc')].count()
+#df[df['persons'] == '2'].count()
+#df[df['status'] == 'unacc'].count()
